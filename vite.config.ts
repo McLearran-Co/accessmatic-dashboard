8bf86852a4399042da0aca491a370bfa847fbd28
John McLearran <hello@mclearran.com>
2025-09-10 14:55:43 -0500

    Update vite.config.ts
    small improvement for the build output

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
   define: {
     'process.env.VITE_API_URL': JSON.stringify('https://accessmatic-backend-production.up.railway.app')
   }
